d97c5e659819fb12f593e2f796689812e5a1919a
Mustafa Neto <mustafaneto26@gmail.com>
2023-11-18 02:54:12 -0300

    improve UI/UX

--- extension.js
+++ extension.js
@@ -1,75 +1,121 @@
-const vscode = require('vscode');
+const vscode = require("vscode");
 
 async function analyzeCode(selectedCode) {
-	try {
-		const OPENAI_API_KEY = "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";
+  try {
+    const OPENAI_API_KEY =
+      "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";
 
-		const request = {
-            model: "gpt-3.5-turbo",
-			messages: [{ role: "system", content: `Search for vulnerabilities in the following code:\n${selectedCode}\nGive me some code solutions. Please answer me in Portuguese` }],
-        };
+    const request = {
+      model: "gpt-3.5-turbo",
+      messages: [
+        {
+          role: "system",
+          content: `Detecte as vulnerabilidades no código e explique, se tiver uma:\n${selectedCode}\n`,
+        },
+      ],
+    };
 
-		vscode.window.withProgress({
-            location: vscode.ProgressLocation.Notification,
-            title: 'Análisando código...',
-            cancellable: false
-        }, async (progress, token) => {
-            progress.report({ increment: 0 });
+    vscode.window.withProgress(
+      {
+        location: vscode.ProgressLocation.Notification,
+        title: "Análisando código...",
+        cancellable: false,
+      },
+      async (progress, token) => {
+        progress.report({ increment: 0 });
 
-            const response = await fetch("https://api.openai.com/v1/chat/completions", {
-                method: "POST",
-                headers: {
-                    Accept: "application/json",
-                    "Content-Type": "application/json",
-                    Authorization: "Bearer " + OPENAI_API_KEY,
-                },
-                body: JSON.stringify(request)
-            });
+        const response = await fetch(
+          "https://api.openai.com/v1/chat/completions",
+          {
+            method: "POST",
+            headers: {
+              Accept: "application/json",
+              "Content-Type": "application/json",
+              Authorization: "Bearer " + OPENAI_API_KEY,
+            },
+            body: JSON.stringify(request),
+          }
+        );
 
-            const json = await response.json();
-            const responseMessage = json['choices'][0]['message']['content'];
+        const json = await response.json();
+        const responseMessage = json["choices"][0]["message"]["content"];
 
-            const panel = vscode.window.createWebviewPanel(
-                'analideDeVulnerabilidade',
-                'Análise de Vulnerabilidade',
-                vscode.ViewColumn.Beside,
-                {}
-            );
+        const panel = vscode.window.createWebviewPanel(
+          "analideDeVulnerabilidade",
+          "Análise de Vulnerabilidade",
+          vscode.ViewColumn.Beside,
+          {}
+        );
 
-            panel.webview.html = `<html><body><h1>Análise de Vulnerabilidade</h1><p>${responseMessage}</p></body></html>`;
-
-            vscode.window.showInformationMessage('Análise de vulnerabilidade completa.');
-        });
-	} catch (error) {
-		console.error('Error analyzing code:', error);
-		vscode.window.showErrorMessage('Error analyzing code: ' + error.message);
-	}
-    
+        panel.webview.html = `
+                                <html>
+                                <head>
+                                    <style>
+                                        body {
+                                            font-family: Arial, sans-serif;
+                                            padding: 10px;
+                                        }
+                                        h1 {
+                                            padding: 10px;
+                                        }
+                                        .content {
+                                            font-size: 18px;
+                                        }
+                                        pre {
+                                            white-space: pre-wrap;
+                                            max-width: 100%;
+                                            overflow: auto;
+                                            padding: 10px;
+                                        }
+                                    </style>
+                                    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.9.0/styles/default.min.css">
+                                    <script src="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.9.0/highlight.min.js"></script>
+                                </head>
+                                <body>
+                                    <h1>Análise de Vulnerabilidade</h1>
+                                    <div class="content">
+                                        <pre><code class="javascript">${responseMessage}</code></pre>
+                                    </div>
+                                    <script>hljs.highlightAll();</script>
+                                </body>
+                                </html>`;
+        vscode.window.showInformationMessage(
+          "Análise de vulnerabilidade completa."
+        );
+      }
+    );
+  } catch (error) {
+    console.error("Error analyzing code:", error);
+    vscode.window.showErrorMessage("Error analyzing code: " + error.message);
+  }
 }
 
 /**
  * @param {vscode.ExtensionContext} context
  */
 function activate(context) {
-	console.log('Congratulations, your extension "code-analyzer" is now active!');
+  console.log('Congratulations, your extension "code-analyzer" is now active!');
 
-	let disposable = vscode.commands.registerCommand('code-analyzer.analyzeCodeCommand', function () {
-		let editor = vscode.window.activeTextEditor;
-		if (!editor) {
-			vscode.window.showErrorMessage('No active text editor found.');
-		} else {
-			let selectedCode = editor.document.getText(editor.selection);
-			analyzeCode(selectedCode);
-		}
-	});
+  let disposable = vscode.commands.registerCommand(
+    "code-analyzer.analyzeCodeCommand",
+    function () {
+      let editor = vscode.window.activeTextEditor;
+      if (!editor) {
+        vscode.window.showErrorMessage("No active text editor found.");
+      } else {
+        let selectedCode = editor.document.getText(editor.selection);
+        analyzeCode(selectedCode);
+      }
+    }
+  );
 
-	context.subscriptions.push(disposable);
+  context.subscriptions.push(disposable);
 }
 
 // This method is called when your extension is deactivated
 function deactivate() {}
 
 module.exports = {
-	activate,
-	deactivate
-}
+  activate,
+  deactivate,
+};
